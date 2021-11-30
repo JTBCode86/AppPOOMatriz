44dc3fb888e054267a4767af7a742901ac4ed5c6
Jean Barbosa <jeanbarbosa.it@gmail.com>
2021-11-29 22:54:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AppPOOMatriz
+{
+    /// <summary>
+    /// Exercicio Matriz
+    /// Neste exercicio, será criada a classe Matriz, deixando as operações de matriz
+    /// sob responsabilidade dela.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre com os valores de uma matriz bidimencional com espaço: ");
+            string[] values = Console.ReadLine().Split(" ");
+
+            int n = int.Parse(values[0]);
+            int m = int.Parse(values[1]);
+            matriz objMatriz = new matriz(n, m);
+            Console.WriteLine();
+
+            Console.WriteLine("Entre com os valores da matriz abaixo: ");
+            objMatriz.preencheMatriz();
+            Console.Clear();
+
+            Console.WriteLine("Os valores atuais são: ");
+            objMatriz.imprimirMatriz();
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Entre com o valor da multiplicados: ");
+            int x = int.Parse(Console.ReadLine());
+            objMatriz.multiplicarMatriz(x);
+            Console.Clear();
+
+            Console.WriteLine("Os valores atuais na matriz são: ");
+            objMatriz.imprimirMatriz();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ matriz.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AppPOOMatriz
+{
+    class matriz
+    {
+        public int n { get; set; }
+        public int m { get; set; }
+
+        public int[,] mat;
+
+        public matriz(int M, int N) 
+        {
+            this.m = M;
+            this.n = N;
+        }
+
+        public void preencheMatriz() 
+        {
+             mat = new int[m,n];
+
+            for (int i = 0; i < m; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
+        }
+
+        public void imprimirMatriz() 
+        {
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public int[,] multiplicarMatriz(int x) 
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    mat[i, j] = mat[i, j] * x;
+                }
+            }
+            return mat;
+        }
+    }
+}
